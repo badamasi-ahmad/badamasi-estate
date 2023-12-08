30ac9d680f961b3414ca1c7abdc9cb170415e490
badamasi ahmad <badamasiahmad106@gmail.com>
2023-12-08 07:50:38 +0100

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import Abaut from "./pages/abaut";
 
 export default function App() {
   
-  return <h1 className='text-red-500'>App</h1>;
+  return <BrowserRouter>
+  <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="Profile" element={<Profile />}/>
+      <Route path="Abaut" element={<Abaut />}/>
+      <Route path="Sign-In" element={<SignIn />}/>
+      <Route path="Sign-Up" element={<SignUp />}/>
+  </Routes>
+  </BrowserRouter>
+
   
 }
