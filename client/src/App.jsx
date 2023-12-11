68fcbfcae93f33485dc6a438fa126c321190bd74
badamasi ahmad <badamasiahmad106@gmail.com>
2023-12-11 20:53:14 +0100

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
-import About from "./pages/about";
+import About from "./pages/About";
 import Header from './components/header';
 
 export default function App() {
