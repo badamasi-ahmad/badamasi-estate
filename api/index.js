87481522e80caad6d9cb67a02213f524558bb9c8
badamasi ahmad <badamasiahmad106@gmail.com>
2023-12-14 09:42:17 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config(),
 
-mongoose.connect(process.env.MONGO).then(() =>{
+mongoose.connect(process.env.MONGO)
+.then(() =>{
     console.log('connected to MongoDB');
-}).catch((err) => {
-    console.log(err)
 })
+.catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 app.listen(3000, () => {
