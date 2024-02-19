46cdb8de4185d268fac8d3b99b07c2f74d292b12
badamasi <badamasiahmad106@gmail.com>
2024-02-20 00:40:34 +0100

    add signout functionality

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import  express  from "express";
-import { SignIn, SignUp, google } from "../controllers/auth.controller.js";
+import { SignIn, SignOut, SignUp, google } from "../controllers/auth.controller.js";
 
 const router =express.Router();
 
 router.post("/SignUp", SignUp);
 router.post("/SignIn", SignIn);
-router.post('/google', google)
+router.post('/google', google);
+router.get('/Signout', SignOut);
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -10,6 +10,9 @@ import {
   deleteUserStart, 
   deleteUserFailure, 
   deleteUserSuccess, 
+  signOutUserFailure,
+  signOutUserSuccess,
+  signOutUserStart,
 } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
@@ -98,6 +101,22 @@ export default function Profile() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      dispatch(signOutUserStart());
+      const res = await fetch('/api/auth/signout');
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(data.message));
+    }
+  };
+
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text3xl font-semibold text-center my-7'> Profile </h1>
@@ -153,7 +172,7 @@ export default function Profile() {
       </form>
       <div className='flex justify-between mt-5'>
         <span onClick={handleDeleteUser}className='text-red-700 cursor-pointer'>Delete account</span>
-        <span className='text-red-700 cursor-pointer'>sign out</span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>sign out</span>
       </div>
       <p className='text-red-700 mt-5'> {error ? error : ''} </p>
       <p className='text-green-700 mt-5'>{updateSuccess ? 'user updated successfully!' : ''} </p>
