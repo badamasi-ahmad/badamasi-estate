45e93c5567e6457aac56e73682b715cc19404542
badamasi <badamasiahmad106@gmail.com>
2024-02-19 23:57:58 +0100

    add delete user funtionality

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express  from "express";
-import { test, updateUser } from "../controllers/user.controller.js";
+import { deleteUser, test, updateUser } from "../controllers/user.controller.js";
 import { varifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 router.get('/test', test);
 router.post('/update/:id', varifyToken, updateUser)
-    
+router.delete('/delete/:id', varifyToken, deleteUser)
+
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -3,7 +3,14 @@ import { useRef, useState, useEffect } from 'react'
 import { app } from '../firebase';
 import {getDownloadURL, getStorage, ref,
    uploadBytesResumable} from 'firebase/storage';
-import { updateUserStart, updateUserSuccess, updateUserFailure, } from '../redux/user/userSlice';
+import { 
+  updateUserStart,
+  updateUserSuccess, 
+  updateUserFailure, 
+  deleteUserStart, 
+  deleteUserFailure, 
+  deleteUserSuccess, 
+} from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 
 export default function Profile() {
@@ -73,6 +80,24 @@ export default function Profile() {
     }
 
   };
+  
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text3xl font-semibold text-center my-7'> Profile </h1>
@@ -127,7 +152,7 @@ export default function Profile() {
         </button>
       </form>
       <div className='flex justify-between mt-5'>
-        <span className='text-red-700 cursor-pointer'>Delete account</span>
+        <span onClick={handleDeleteUser}className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>sign out</span>
       </div>
       <p className='text-red-700 mt-5'> {error ? error : ''} </p>
